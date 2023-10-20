37b682fd63e8a0cc0e1370df2e827c2822403ae4
Artyom Koshko <artyomkoshko@gmail.com>
2023-10-20 22:52:38 +0300

    fix: update swagger url

--- backend/EmerchAPI/EmerchAPI/Program.cs
+++ backend/EmerchAPI/EmerchAPI/Program.cs
@@ -9,12 +9,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "api";
+});
 
 app.UseHttpsRedirection();
 
