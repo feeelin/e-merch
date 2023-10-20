8965cf941f34969202e5eb8a44dd722942030efc
Artyom Koshko <artyomkoshko@gmail.com>
2023-10-21 00:02:11 +0300

    regenerate frontend client

--- frontend/src/emerch.client.ts
+++ frontend/src/emerch.client.ts
@@ -10,7 +10,7 @@
 
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelToken } from 'axios';
 
-export class Client {
+export class EmerchApiClient {
     private instance: AxiosInstance;
     private baseUrl: string;
     protected jsonParseReviver: ((key: string, value: any) => any) | undefined = undefined;
